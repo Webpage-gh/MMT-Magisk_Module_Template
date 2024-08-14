cd05549481c4b35778bfee81766cd9e56c2ab49b
Webpage <70434775+Webpage-gh@users.noreply.github.com>
2024-08-14 17:41:07 +0800

    Re-format README.md

--- README.md
+++ README.md
@@ -27,29 +27,29 @@ REMOVE="
 **KernelSU 和 APatch 特有功能**，将会把 `/system/app/YouTube` 和 `/system/app/Bloatware` 删除。禁用模块后重启恢复。
 
 ## 通用变量
-MODPATH (path): 当前模块的安装目录
-TMPDIR (path): 可以存放临时文件的目录
-ZIPFILE (path): 当前模块的安装包文件
-ARCH (string): 设备的 CPU 构架，只有 arm64
-IS64BIT (bool): 是否是 64 位设备
-API (int): 当前设备的 Android API 版本 (如：Android 6.0 上为 23)
+- MODPATH (path): 当前模块的安装目录
+- TMPDIR (path): 可以存放临时文件的目录
+- ZIPFILE (path): 当前模块的安装包文件
+- ARCH (string): 设备的 CPU 构架，只有 arm64
+- IS64BIT (bool): 是否是 64 位设备
+- API (int): 当前设备的 Android API 版本 (如：Android 6.0 上为 23)
 
 ### KernelSU 变量
-KSU (bool): 标记此脚本运行在 KernelSU 环境下，此变量的值将永远为 true，你可以通过它区分 Magisk。
-KSU_VER (string): KernelSU 当前的版本名字 (如： v0.4.0)
-KSU_VER_CODE (int): KernelSU 用户空间当前的版本号 (如 10672)
-KSU_KERNEL_VER_CODE (int): KernelSU 内核空间当前的版本号 (如 10672)
-BOOTMODE (bool): 此变量在 KernelSU 中永远为 true
+- KSU (bool): 标记此脚本运行在 KernelSU 环境下，此变量的值将永远为 true，你可以通过它区分 Magisk。
+- KSU_VER (string): KernelSU 当前的版本名字 (如： v0.4.0)
+- KSU_VER_CODE (int): KernelSU 用户空间当前的版本号 (如 10672)
+- KSU_KERNEL_VER_CODE (int): KernelSU 内核空间当前的版本号 (如 10672)
+- BOOTMODE (bool): 此变量在 KernelSU 中永远为 true
 
 ### APATCH 变量
-KERNELPATCH (bool): 标记此脚本运行在 APatch 环境下，此变量的值将永远为 true
-KERNEL_VERSION (hex): 从 KernelPatch 继承，内核版本号 (如： 50a01 是指 5.10.1)
-KERNELPATCH_VERSION (hex): 从 KernelPatch 继承，KernelPatch 版本号 (如： a05 是指 0.10.5)
-SUPERKEY (string): 从 KernelPatch 继承，用于调用 kpatch 或者 supercall
-APATCH (bool): 标记此脚本运行在 APatch 环境下，此变量的值将永远为 true
-APATCH_VER_CODE (int): APatch 当前的版本号 (如 10672)
-APATCH_VER (string): APatch 当前的版本名 (如 10672)
-BOOTMODE (bool): 此变量在 APatch 中永远为 true
+- KERNELPATCH (bool): 标记此脚本运行在 APatch 环境下，此变量的值将永远为 true
+- KERNEL_VERSION (hex): 从 KernelPatch 继承，内核版本号 (如： 50a01 是指 5.10.1)
+- KERNELPATCH_VERSION (hex): 从 KernelPatch 继承，KernelPatch 版本号 (如： a05 是指 0.10.5)
+- SUPERKEY (string): 从 KernelPatch 继承，用于调用 kpatch 或者 supercall
+- APATCH (bool): 标记此脚本运行在 APatch 环境下，此变量的值将永远为 true
+- APATCH_VER_CODE (int): APatch 当前的版本号 (如 10672)
+- APATCH_VER (string): APatch 当前的版本名 (如 10672)
+- BOOTMODE (bool): 此变量在 APatch 中永远为 true
 
 ## 内置函数
 ```sh
@@ -61,7 +61,7 @@ ui_print() {
   fi
 }
 ```
-例: ui_print "- 正在设置权限"
+例: `ui_print "- 正在设置权限"`  
 确保 Recovery 模式下正确打印文字到屏幕上，尽管 KernelSU 和 APatch 不支持 Recovery 模式下安装模块，但习惯上依然使用 ui_print 代替 echo
 
 ```sh
@@ -73,7 +73,7 @@ abort() {
   exit 1
 }
 ```
-例: abort "! 不支持的架构: $ARCH"
+例: `abort "! 不支持的架构: $ARCH"`  
 abort 比 exit 会多做一个清理的步骤，这也是我们应该避免使用 exit 的原因
 
 ```sh
@@ -89,7 +89,7 @@ set_perm() {
   chcon "$selinux_context" "$file_path" || return 1
 }
 ```
-例: set_perm /data/adb/modules/post-fs-data.d/example.sh root root 755
+例: `set_perm /data/adb/modules/post-fs-data.d/example.sh root root 755`  
 设置文件权限所有者、用户组、权限和 SELinux 上下文，SELinux 上下文为可选项
 
 ```sh
@@ -109,8 +109,10 @@ set_perm_recursive() {
 }
 ```
 例: 
+```
 pkg=bin.mt.plus
 user=$(dumpsys package $pkg | sed -n 's/.*userId=\([0-9]*\).*/\1/p')
 group=$user
 set_perm_recursive "/data/user/0/$pkg" $user $group 771 660
+```
 递归设置目录及其内容的所有权、权限和上下文
--- customize.sh
+++ customize.sh
@@ -4,7 +4,7 @@
 在第三行开始编写代码，编写完成后删除下面的参考文章，包括本行；customize意为自定义，这个脚本的作用是在安装模块时执行一些命令；第一行成为shebang，相当于“方言”，它可以为人们和机器指示Shell解释器的类型，建议保留
 
 ## 调试代码
-Root管理器会内置一个busybox，避免手机厂商的改动影响代码，让脚本始终在可预测的环境中执行。要模拟这个环境，请在终端中执行以下命令（建议复制粘贴避免出错）
+Root 管理器会内置一个 busybox，避免手机厂商的改动影响代码，让脚本始终在可预测的环境中执行。要模拟这个环境，请在终端中执行以下命令（建议复制粘贴避免出错）
 ```POSIX sh
 /system/bin/su -c 'for i in magisk ksu/bin ap/bin; do i=/data/adb/$i/busybox; if [ -x "$i" ]; then ASH_STANDALONE=1 $i sh; break; fi; done'
 ```
@@ -12,7 +12,7 @@ Root管理器会内置一个busybox，避免手机厂商的改动影响代码，
 Magisk: `ASH_STANDALONE=1 /data/adb/magisk/busybox sh <sh脚本>`
 KernelSU: `ASH_STANDALONE=1 /data/adb/ksu/bin/busybox sh <sh脚本>`
 APatch: `ASH_STANDALONE=1 /data/adb/ap/bin/busybox sh <sh脚本>`
-其中 <sh脚本> 为可选项，不填写时会打开一个Shell。
+其中 <sh脚本> 为可选项，不填写时会打开一个 Shell。
 
 ## 有用的变量
 REPLACE="
@@ -27,29 +27,29 @@ REMOVE="
 **KernelSU 和 APatch 特有功能**，将会把 `/system/app/YouTube` 和 `/system/app/Bloatware` 删除。禁用模块后重启恢复。
 
 ## 通用变量
-MODPATH (path): 当前模块的安装目录
-TMPDIR (path): 可以存放临时文件的目录
-ZIPFILE (path): 当前模块的安装包文件
-ARCH (string): 设备的 CPU 构架，只有 arm64
-IS64BIT (bool): 是否是 64 位设备
-API (int): 当前设备的 Android API 版本 (如：Android 6.0 上为 23)
+- MODPATH (path): 当前模块的安装目录
+- TMPDIR (path): 可以存放临时文件的目录
+- ZIPFILE (path): 当前模块的安装包文件
+- ARCH (string): 设备的 CPU 构架，只有 arm64
+- IS64BIT (bool): 是否是 64 位设备
+- API (int): 当前设备的 Android API 版本 (如：Android 6.0 上为 23)
 
 ### KernelSU 变量
-KSU (bool): 标记此脚本运行在 KernelSU 环境下，此变量的值将永远为 true，你可以通过它区分 Magisk。
-KSU_VER (string): KernelSU 当前的版本名字 (如： v0.4.0)
-KSU_VER_CODE (int): KernelSU 用户空间当前的版本号 (如 10672)
-KSU_KERNEL_VER_CODE (int): KernelSU 内核空间当前的版本号 (如 10672)
-BOOTMODE (bool): 此变量在 KernelSU 中永远为 true
+- KSU (bool): 标记此脚本运行在 KernelSU 环境下，此变量的值将永远为 true，你可以通过它区分 Magisk。
+- KSU_VER (string): KernelSU 当前的版本名字 (如： v0.4.0)
+- KSU_VER_CODE (int): KernelSU 用户空间当前的版本号 (如 10672)
+- KSU_KERNEL_VER_CODE (int): KernelSU 内核空间当前的版本号 (如 10672)
+- BOOTMODE (bool): 此变量在 KernelSU 中永远为 true
 
 ### APATCH 变量
-KERNELPATCH (bool): 标记此脚本运行在 APatch 环境下，此变量的值将永远为 true
-KERNEL_VERSION (hex): 从 KernelPatch 继承，内核版本号 (如： 50a01 是指 5.10.1)
-KERNELPATCH_VERSION (hex): 从 KernelPatch 继承，KernelPatch 版本号 (如： a05 是指 0.10.5)
-SUPERKEY (string): 从 KernelPatch 继承，用于调用 kpatch 或者 supercall
-APATCH (bool): 标记此脚本运行在 APatch 环境下，此变量的值将永远为 true
-APATCH_VER_CODE (int): APatch 当前的版本号 (如 10672)
-APATCH_VER (string): APatch 当前的版本名 (如 10672)
-BOOTMODE (bool): 此变量在 APatch 中永远为 true
+- KERNELPATCH (bool): 标记此脚本运行在 APatch 环境下，此变量的值将永远为 true
+- KERNEL_VERSION (hex): 从 KernelPatch 继承，内核版本号 (如： 50a01 是指 5.10.1)
+- KERNELPATCH_VERSION (hex): 从 KernelPatch 继承，KernelPatch 版本号 (如： a05 是指 0.10.5)
+- SUPERKEY (string): 从 KernelPatch 继承，用于调用 kpatch 或者 supercall
+- APATCH (bool): 标记此脚本运行在 APatch 环境下，此变量的值将永远为 true
+- APATCH_VER_CODE (int): APatch 当前的版本号 (如 10672)
+- APATCH_VER (string): APatch 当前的版本名 (如 10672)
+- BOOTMODE (bool): 此变量在 APatch 中永远为 true
 
 ## 内置函数
 ```sh
@@ -61,7 +61,7 @@ ui_print() {
   fi
 }
 ```
-例: ui_print "- 正在设置权限"
+例: `ui_print "- 正在设置权限"`  
 确保 Recovery 模式下正确打印文字到屏幕上，尽管 KernelSU 和 APatch 不支持 Recovery 模式下安装模块，但习惯上依然使用 ui_print 代替 echo
 
 ```sh
@@ -73,7 +73,7 @@ abort() {
   exit 1
 }
 ```
-例: abort "! 不支持的架构: $ARCH"
+例: `abort "! 不支持的架构: $ARCH"`  
 abort 比 exit 会多做一个清理的步骤，这也是我们应该避免使用 exit 的原因
 
 ```sh
@@ -89,7 +89,7 @@ set_perm() {
   chcon "$selinux_context" "$file_path" || return 1
 }
 ```
-例: set_perm /data/adb/modules/post-fs-data.d/example.sh root root 755
+例: `set_perm /data/adb/modules/post-fs-data.d/example.sh root root 755`  
 设置文件权限所有者、用户组、权限和 SELinux 上下文，SELinux 上下文为可选项
 
 ```sh
@@ -109,8 +109,10 @@ set_perm_recursive() {
 }
 ```
 例: 
+```
 pkg=bin.mt.plus
 user=$(dumpsys package $pkg | sed -n 's/.*userId=\([0-9]*\).*/\1/p')
 group=$user
 set_perm_recursive "/data/user/0/$pkg" $user $group 771 660
+```
 递归设置目录及其内容的所有权、权限和上下文
